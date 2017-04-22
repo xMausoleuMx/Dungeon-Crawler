4ed2672ca8057ff8d9948cd697498a13858c1a91
xMausoleuMx <j0hntw1ll1ams95@gmail.com>
2017-04-21 22:06:33 -0500

    Added basic functionality to the dungeon generation(WIP) and added a basic GUI(WIP)

---
+++ GUI.java
@@ -0,0 +1,42 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+
+public class GUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI() {
+		setResizable(false);
+		setTitle("Dungeon Crawler");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 639, 480);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+}
--- World.java
+++ World.java
@@ -1,13 +1,58 @@
+import java.util.*;
 
 public class World {
 	private int size, difficulty;
 	
-	public World(int s, int d){
-		size = s;
+	public World(int d){
 		difficulty = d;
 	}
 	
 	public void genWorld(){
+		Random num = new Random();
+		size = num.nextInt(3+difficulty);
+		Room start = new Room(false);
+		ArrayList<Room> roomHolder = new ArrayList<Room>();
+		roomHolder.add(start);
 		
+		//this generates rooms with monsters and items.
+		for(int i =0; i < size; i++){
+			boolean isExit;
+			int z = num.nextInt(2);
+			if(z == 1)
+				isExit = true;
+			else
+				isExit = false;
+				
+			Room temp = new Room(isExit);
+			roomHolder.add(temp);
+		}
+	}
+}
+
+
+class Room{
+	private Room north, south, east, west;
+	public Monster resident;
+	private boolean hasStairs;
+	public Item loot;
+	public Room(boolean flag){
+		hasStairs = flag;
+	}
+	
+	public void connectRoom(Room temp, int direction){
+		switch(direction){
+		case 1:
+			north = temp;
+			break;
+		case 2:
+			east = temp;
+			break;
+		case 3:
+			south = temp;
+			break;
+		case 4:
+			west = temp;
+			break;
+		}
 	}
 }
